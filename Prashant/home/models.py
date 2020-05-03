39a71d485368668a0ba481d5db4dd43519c2a457
Prashant Mudgal <55448739+mudgalprashant@users.noreply.github.com>
2020-05-03 09:57:27 +0530

    Update models.py

--- Prashant/home/models.py
+++ Prashant/home/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 class Profile(models.Model):
+	
 	username = models.CharField(max_length = 100)
 	name = models.CharField( max_length = 50, default= 'New User')
 	email = models.EmailField(max_length = 100)
 	bio = models.TextField( max_length = 150, default = 'Lets make some effort')
 	age = models.IntegerField(default = 18)
-	gender = models.CharField( max_length = 15,null = True)
+	gender = models.CharField( max_length = 15, default = 'Other')
 	address = models.TextField(null = True)
 	contact = models.BigIntegerField(null = True)
 	profession = models.CharField(max_length = 100, null = True)
 	my_drives = ArrayField(models.CharField(max_length = 100, null=True), size = 50, null = True)
-	image = models.ImageField(upload_to='images/', blank = True)
+	image = models.ImageField(upload_to='profile_pics', default = 'avatar2.png')
 
 class Drives(models.Model):
 	creator = models.CharField(max_length = 50, null = True)
