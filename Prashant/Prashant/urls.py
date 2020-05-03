9a8e3b2fd3667ce585256069065a03c372832bb4
Prashant Mudgal <55448739+mudgalprashant@users.noreply.github.com>
2020-05-03 10:00:06 +0530

    Update urls.py

--- Prashant/Prashant/urls.py
+++ Prashant/Prashant/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('home.urls'))
 ]
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
