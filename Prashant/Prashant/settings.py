ad4e92878bbb1a6116c470d86d3e091c04f6ecc0
Prashant Mudgal <55448739+mudgalprashant@users.noreply.github.com>
2020-05-03 10:06:43 +0530

    Update settings.py

--- Prashant/Prashant/settings.py
+++ Prashant/Prashant/settings.py
@@ -123,5 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/images/'
