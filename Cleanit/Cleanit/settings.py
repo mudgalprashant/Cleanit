a229886f91d2624dfc465f754cb88c67a8d9124f
Prashant Mudgal <55448739+mudgalprashant@users.noreply.github.com>
2020-09-30 13:14:43 +0530

    Update settings.py

--- Cleanit/Cleanit/settings.py
+++ Cleanit/Cleanit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')um-3!i)9up#+i^83419@7a1_x*xc+(&a57+w27-8sf-qzie_i'
+SECRET_KEY = '**************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
